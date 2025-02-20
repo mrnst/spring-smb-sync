99a88512a5dcc23ebb8f52b34af5f53accb550d0
mrnst <74326070+mrnst@users.noreply.github.com>
2025-02-20 20:36:09 +0100

    Initial commit

---
+++ src/main/java/smbsync/LocalToSmbFileTransferConfiguration.java
@@ -0,0 +1,57 @@
+package smbsync;
+
+import jcifs.DialectVersion;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.integration.file.dsl.Files;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.handler.advice.ExpressionEvaluatingRequestHandlerAdvice;
+import org.springframework.integration.smb.dsl.Smb;
+import org.springframework.integration.smb.session.SmbSessionFactory;
+
+import java.io.File;
+
+@Configuration
+public class LocalToSmbFileTransferConfiguration {
+
+    @Bean
+    public SmbSessionFactory smbSessionFactory() {
+        final var smbSessionFactory = new SmbSessionFactory();
+        smbSessionFactory.setHost("myHost");
+        smbSessionFactory.setPort(445);
+        smbSessionFactory.setDomain("myDomain");
+        smbSessionFactory.setUsername("myUsername");
+        smbSessionFactory.setPassword("myPassword");
+        smbSessionFactory.setShareAndDir("/myShareAndDir");
+        smbSessionFactory.setSmbMinVersion(DialectVersion.SMB210);
+        smbSessionFactory.setSmbMaxVersion(DialectVersion.SMB311);
+        return smbSessionFactory;
+    }
+
+    @Bean
+    public IntegrationFlow fileToSmbSyncFlow() {
+        return IntegrationFlow.from(
+                        Files.inboundAdapter(new File("myLocalDirectory"))
+                                .preventDuplicates(false),
+                        e -> e.poller(Pollers.fixedDelay(1000).maxMessagesPerPoll(-1))
+                )
+                .transform(Files.toStringTransformer())
+                .handle(Smb.outboundAdapter(smbSessionFactory(), FileExistsMode.REPLACE)
+                                .useTemporaryFileName(true)
+                                .remoteDirectory("/remoteDirectory"),
+                        e -> e.advice(originalFileDeletionAdvice())
+                )
+                .get();
+    }
+
+    @Bean
+    public ExpressionEvaluatingRequestHandlerAdvice originalFileDeletionAdvice() {
+        final var advice = new ExpressionEvaluatingRequestHandlerAdvice();
+        advice.setOnSuccessExpressionString("headers['" + FileHeaders.ORIGINAL_FILE + "'].delete()");
+        advice.setOnFailureExpressionString("headers['" + FileHeaders.ORIGINAL_FILE + "'].delete()");
+        return advice;
+    }
+}
